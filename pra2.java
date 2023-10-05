7f54dfbdf9b6bf305209eb7d71cb326bd5dbc621
Sasaki kaisei <k.sasaki.sys23@morijyobi.ac.jp>
2023-10-05 15:09:54 +0900

    pra2.javaのスペルミスの訂正
    Wourld→World

--- pra2.java
+++ pra2.java
@@ -1,7 +1,7 @@
 public classe pra2{
 	public static void main(String[] args){
 		   for (int i = 1; i <= 100; i++){
-		System.out,println("Hello Wourld");
+		System.out,println("Hello World");
 	}
 }
 }
